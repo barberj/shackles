a989090a9f6c7292c120413a64e5c42a4274327c
barberj <barber.justin@gmail.com>
2012-07-22 17:47:37 -0400

    update comments, plan walk generator method

--- shackles.py
+++ shackles.py
@@ -1,22 +1,25 @@
-def broken(object, attr_chain):
+def broken(obj, chain):
     """Return name of attribute where the chain
     is broken.
 
-    If chain is unbroken nothing is returned."""
+    If chain is not broken (all attributes are represented in chain)
+    nothing is returned."""
 
-    if isinstance(attr_chain, (str, unicode)):
-        attr_chain = attr_chain.split('.')
+    if isinstance(chain, (str, unicode)):
+        chain = chain.split('.')
 
-    if isinstance(attr_chain, (list, tuple)):
-        for attr in attr_chain:
-            object = getattr(object, attr, None)
-            if not object:
+    if isinstance(chain, (list, tuple)):
+        for attr in chain:
+            obj = getattr(obj, attr, None)
+            if not obj:
                 return attr
 
-def value(object, attr_chain):
-    """Recursively gets attributes in chain.
-    If at any point in the chain a link is missing
-    None is returned.
+def getattr(obj, chain, default=None):
+    """Recursively walk chain. Return the value
+    of the final named attribute in the chain.
+
+    If a named attribute does not exist,
+    default is returned if provided, otherwise AttributeError is raised.
 
     Replaces doing things like:
     object.attr1.attr2.attr3 if object and object.attr1.attr2 else None
@@ -26,12 +29,15 @@ def value(object, attr_chain):
     getattr(getattr(getattr(object, 'attr1', None), 'attr2', None), 'attr3', None)
     """
 
-    if isinstance(attr_chain, (str, unicode)):
-        attr_chain = attr_chain.split('.')
+    if isinstance(chain, (str, unicode)):
+        chain = chain.split('.')
 
-    if isinstance(attr_chain, (list, tuple)):
-        for attr in attr_chain:
-            object = getattr(object, attr, None)
-            if not object:
+    if isinstance(chain, (list, tuple)):
+        for attr in chain:
+            obj = getattr(obj, attr, None)
+            if not obj:
                 break
-    return object
+    return obj
+
+def walk(obj, chain):
+    pass
