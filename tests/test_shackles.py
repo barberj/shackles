b40130b601bbf1381e0a0a737eb820a5c7d6930e
barberj <barber.justin@gmail.com>
2012-07-22 18:21:08 -0400

    shackles.get tests

--- tests/test_shackles.py
+++ tests/test_shackles.py
@@ -3,26 +3,40 @@ import shackles
 
 class prisoner(object):
     """Dummy object to shackle to for testing"""
-    pass
+    def __init__(self, name=None):
+        self.name = name
 
 class test_shackles(TestCase):
 
     def setUp(self):
         self.obj = prisoner()
-        self.obj.a = prisoner()
-        self.obj.a.b = prisoner()
+        self.obj.a = prisoner('a')
+        self.obj.a.b = prisoner('b')
+        self.obj.a.b.e = prisoner('e')
 
     def test_broken_chain_str(self):
-
+        assert shackles.broken(self.obj, 'e') is 'e'
         assert shackles.broken(self.obj, 'a.b') is None
         assert shackles.broken(self.obj, 'a.b.c') == 'c'
 
     def test_broken_chain_list(self):
-
+        assert shackles.broken(self.obj, ['e']) is 'e'
         assert shackles.broken(self.obj, ['a','b']) is None
         assert shackles.broken(self.obj, ['a','b','c']) == 'c'
 
     def test_broken_chain_tuple(self):
-
+        assert shackles.broken(self.obj, ('e')) is 'e'
         assert shackles.broken(self.obj, ('a','b')) is None
         assert shackles.broken(self.obj, ('a','b','c')) == 'c'
+
+    def test_get_str(self):
+        assert shackles.get(self.obj, 'a.b').name == 'b'
+        assert shackles.get(self.obj, 'a.b.name') == 'b'
+
+    def test_get_list(self):
+        assert shackles.get(self.obj, ['a','b']).name == 'b'
+        assert shackles.get(self.obj, ['a','b','name']) == 'b'
+
+    def test_get_tuple(self):
+        assert shackles.get(self.obj, ('a','b')).name == 'b'
+        assert shackles.get(self.obj, ('a','b','name')) == 'b'
